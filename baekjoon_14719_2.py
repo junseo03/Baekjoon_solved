490dd487b7d094fcd32ff5404d89595e16dc9cec
junseo03 <wjdwnstj0913@gmail.com>
2022-09-09 21:16:12 +0900

    solved_2

---
+++ baekjoon_14719_2.py
@@ -0,0 +1,13 @@
+H,W = map(int,input().split())
+block = list(map(int,input().split()))
+answer = 0
+
+for i in range(1,W-1):
+    left = max(block[:i])
+    right = max(block[i+1:])
+    m = min(left,right)
+
+    if m > block[i]:
+        answer += m - block[i]
+
+print(answer)
