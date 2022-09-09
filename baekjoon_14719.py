5e280a918cd3d50030832f2b6dee3aa186fa41dc
junseo03 <wjdwnstj0913@gmail.com>
2022-09-09 16:23:46 +0900

    incompleted

---
+++ baekjoon_14719.py
@@ -0,0 +1,19 @@
+H , W = map(int,(input().split()))
+block = list(map(int,(input().split())))
+check = False
+tmp = 0
+rst = 0
+
+for i in range(H,0,-1):
+    for j in range(W):
+        if block[j] >= i:
+            if not check:
+                tmp = j
+                check = True
+                #print(j)
+            elif check:
+                rst += j - tmp - 1
+                check = False
+    check = False
+
+print(rst)
